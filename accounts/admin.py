4d0f2bf75d8ba705a240390eacea3f7023ad1b81
prashantha sp <prashanth.sp98@gmail.com>
2019-11-27 22:53:07 +0530

    Added Events model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ImageSlider, JobOpenings, Account, BestAlumnis
+from .models import ImageSlider, JobOpenings, Account, BestAlumnis, Events
 
 admin.site.register(ImageSlider)
 admin.site.register(JobOpenings)
 admin.site.register(Account)
 admin.site.register(BestAlumnis)
+admin.site.register(Events)
