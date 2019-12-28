ae55dc439b7c0ce7a051759234e36905a674dc3b
prashantha sp <prashanth.sp98@gmail.com>
2019-12-28 14:55:34 +0530

    Updated some environment variable value which cause error while deploying

--- alumni/settings.py
+++ alumni/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL']
-EMAIL_HOST_PASSWORD = os.environ['PASSWD'] 
+EMAIL_HOST_USER = "prashanthsp6498@gmail.com"
+EMAIL_HOST_PASSWORD = '<Prashanthsp@20>' 
